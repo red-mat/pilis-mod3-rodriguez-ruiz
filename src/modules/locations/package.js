98d7c7b9b753499f247b201b3621bc03d43a48f2
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-05 20:43:12 -0300

    feature(module): create package export in locations

---
+++ src/modules/locations/package.js
@@ -0,0 +1,7 @@
+import LocationsProvider from './context/locations-context'
+import withContextLocationsList from './hoc/context-locations-list'
+
+export {
+    LocationsProvider,
+    withContextLocationsList
+}
