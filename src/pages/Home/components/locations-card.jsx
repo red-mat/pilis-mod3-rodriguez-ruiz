3eb2c3d4cad00396a2e5e2362839f52b98c6595d
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-14 21:51:11 -0300

    example

---
+++ src/pages/Home/components/locations-card.jsx
@@ -0,0 +1,9 @@
+// importamos el componente base
+import {LocationCard as Lc} from '../../../components/package';
+
+const LocationCard = () => { 
+    // context o hooks
+
+    // retorna locations cards con los datos
+    return (<LocationCard />)
+ }
