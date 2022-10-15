c9fc2fb749b7325685e9836bb08e92263db313e8
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-15 05:56:11 -0300

    feature: location page terminate

--- src/components/package.js
+++ src/components/package.js
@@ -1,7 +1,13 @@
 import Container from "./container-component/container-component";
 import NavLinkButton from './nav-link/nav-link';
+import Button from './button/button'
+import ErrorMessage from './error-message/error-message';
+import LocationForm from "./location_form/location-form";
 
 export {
     Container,
-    NavLinkButton as NavLink
+    NavLinkButton as NavLink,
+    Button,
+    ErrorMessage,
+    LocationForm
 }
--- src/pages/addLocation/add-location.jsx
+++ src/pages/addLocation/add-location.jsx
@@ -1,7 +1,20 @@
+import './add-location.css';
+
+import { useState } from 'react';
+import {LocationForm, ErrorMessage} from '../../components/package';
+
 const AddLocation = () => {
-    return (<main>
-        add locations
-    </main>)
-}
+ 
+  const [data, setData] = useState('');
+  const handle = (data) => { 
+    console.log(data);
+   }
+  return (
+    <main>
+        <LocationForm handle={handle}/>
+        <ErrorMessage isError={true}>hola mundo</ErrorMessage>
+    </main>
+  );
+};
 
 export default AddLocation;
