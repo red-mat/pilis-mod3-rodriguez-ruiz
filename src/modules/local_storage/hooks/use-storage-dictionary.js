af25bb5025ffa73fcfbe386aaf9286b8d0c38f9a
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-15 21:17:10 -0300

    refactor(module): use storage dictionary

--- src/modules/local_storage/hooks/use-storage-dictionary.js
+++ src/modules/local_storage/hooks/use-storage-dictionary.js
@@ -1,38 +1,47 @@
-import { get } from 'react-hook-form';
 import useLocalStorage from './use-storage';
 
 const useDictionaryStorage = (key) => {
-  const [item, setItem, clearItem] = useLocalStorage(key, {});
+  const [dic, setDic, clear] = useLocalStorage(key, {});
 
   const add = (key, element) => {
-    if (item[key]) throw new Error('exist element');
-    setItem((item) => ({ ...item, key: element }));
+    if (dic[`${key}`]) throw new Error('exist element');
+    
+    setDic((dic) => {
+      const newDic = {...dic};
+      newDic[`${key}`]=element;
+
+      return newDic;
+    });
   };
   const dell = (key) => {
-    if (!item[key])  throw new Error('not exist item');
-    setItem((item) => {
-      let {key, ...newItem} = item
-      return newItem
+    if (dic[`${key}`])  throw new Error('not exist item');
+
+    setDic((dic) => {
+      let {key, ...newDic} = dic
+      return newDic;
     })  
   }
 
   const get = (key) => { 
-    if (!item[key])  throw new Error('not exist item');
-    return item[key]
+    if (!dic[`${key}`])  throw new Error('not exist item');
+
+    return dic[`${key}`]
    }
 
    const set = (key, element) => {
-    if (!item[key])  throw new Error('not exist item');
-    setItem((item) => {
-      item[key]=element
+    if (!dic[`${key}`])  throw new Error('not exist item');
 
-      return item;
+    setDic((dic) => {
+      const newDic = {...dic};
+      newDic[`${key}`]=element
+
+      return newDic;
     })
    }
 
-   const keys = () =>  Object.keys(item);
+   const keys = () =>  Object.keys(dic);
 
-   return {item, clearItem, add, dell, get, set, keys}
+   return {dic, clear, add, dell, get, set, keys}
 };
 
 export default useDictionaryStorage
