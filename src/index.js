1de71f07514042655a97d5dd3cc74da899d4aa6a
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-15 22:13:15 -0300

    feature: implement locations provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import LocationsProvider from './modules/locations/context/locations-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <LocationsProvider>
+        <App />
+      </LocationsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
