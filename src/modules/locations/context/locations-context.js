b8256ee15df035b6c084825aa4c31dbd725125d6
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-15 22:10:37 -0300

    feature(module): use dictionary locations context

--- src/modules/locations/context/locations-context.js
+++ src/modules/locations/context/locations-context.js
@@ -1,8 +1,7 @@
-import { createContext } from "react";
-import useLocalStorage from "../../local_storage/storage-hook";
+import { createContext, useState } from "react";
+import { useDictionaryStorage } from "../../local_storage/hooks/package";
 
 const KEY_STORAGE='locations'
-const DEFAULT_VALUE=[];
 
 export const LocationsContext = createContext({
     locations:[],
@@ -10,9 +9,41 @@ export const LocationsContext = createContext({
 })
 
 const LocationsProvider = ({ children }) => {
-    const [locations, setLocations] = useLocalStorage(KEY_STORAGE, DEFAULT_VALUE);
-    const value = {locations, setLocations}
-  
+    const [state, setState] = useState({error:''});
+
+    const {clear, dic, keys, add, dell, get, set} =  useDictionaryStorage(KEY_STORAGE);
+
+    const addLocation = (location) => {
+        const {latitude, longitude} = location;
+        // comprobar los datos de locations
+        try {
+            add(
+                `${latitude}${longitude}`,
+                location
+            )
+        } catch (error) {
+            setState( state =>{
+                const newState = {...state};
+                newState.error = error;
+                return newState;
+            })
+        }
+    }
+    
+    const dellLocation = (id) => {}
+    const getLocation = (id) => {}
+    const setLocation = (id, location) => {}
+
+    const value = {
+        state,
+        clear,
+        locations:dic,
+        locationsID:keys,
+        addLocation,
+        dellLocation,
+        getLocation,
+        setLocation,
+    }
     return <LocationsContext.Provider value={value}>{children}</LocationsContext.Provider>;
 }
 
