e988e1721e0d15f502f19adbdd3b109afa931c66
adriana2022 <avr2783@gmail.com>
2022-10-18 22:34:51 -0300

    Actualizacion de home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,18 @@
+import { Button } from "react-bootstrap";
 import LocationCards from "../../components/LocationCard/LocationCards";
 import './home.css';
 
 const Home = () => {
     return (
-        <main className="text-center">
+        <div className="text-center">
             <h1>Lista de ubicaciones</h1>
-            <LocationCards></LocationCards>
-        </main>
+            <div className="header p-5">
+                <Button variant="primary">Agregar ubicación</Button>
+            </div>
+            <div>
+                <LocationCards />
+            </div>
+        </div>
     )
 }
 
